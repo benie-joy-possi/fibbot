f96b55912b2b6ac7044a6d3fabbb2073a2d76c9b
benie-joy-possi <beniejoypossi@gmail.com>
2025-02-27 18:19:50 +0100

    modified the main.rs and added the parse.rs file to implement the input parsing and tests for it

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,61 @@
-use std::env;
 
+
+use parse::parse;
+mod parse;
 fn main() {
     println!("Hello, World!");
-    let enable_fib = env::args().nth(1).unwrap_or_else(|| "true".to_string());
 
-    let max_threshold = env::args().nth(2).and_then(|s| s.parse::<u32>().ok()).unwrap_or(1000);
+    match parse() {
+        Ok((enable_fib, max_threshold)) => {
+            println!("Hello, World!");
 
-   
-    println!("enable_fib: {:?}, max_threshold: {:?}", enable_fib, max_threshold)
+            println!(
+                "enable_fib: {:?}, max_threshold: {:?}",
+                enable_fib, max_threshold
+            );
+
+            if enable_fib {
+                println!("fibbonacci genration is up to {}", max_threshold);
+            } else {
+                println!("fibonacci generation has been disabled");
+            }
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
+    {}
+    // let (enable_fib, max_threshold) = parse();
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_inputs_true_1000() {
+        use std::env;
+
+        env::set_var("enable_fib", "true");
+        env::set_var("max_threshold", "1000");
+        let result = parse();
+        assert!(result.is_ok());
+
+        let (enable_fib, max_threshold) = result.unwrap();
+        assert!(enable_fib);
+        assert_eq!(max_threshold, 1000);
+    }
+
+    #[test]
+    fn test_parse_inputs_false_200() {
+        use std::env;
+
+        env::set_var("enable_fib", "false");
+        env::set_var("max_threshold", "200");
+        let result = parse();
+        assert!(result.is_ok());
+
+        let (enable_fib, max_threshold) = result.unwrap();
+        assert!(!enable_fib);
+        assert_eq!(max_threshold, 200);
+    }
 }
---
+++ src/parse.rs
@@ -0,0 +1,22 @@
+use std::{env, format};
+
+pub fn parse() -> Result<(bool, u64), String> {
+    let enable_fib = env::var("enable_fib").unwrap_or_else(|_| "false".to_string());
+
+    let max_threshold = env::var("max_threshold").unwrap_or_else(|_| "1000".to_string());
+
+    let enable_fib: bool = match enable_fib.parse() {
+        Ok(val) => val,
+        Err(_) => {
+           return Err(format!("Invalid input of enable_fib {}", enable_fib));
+        }
+    };
+
+    let max_threshold: u64 = match max_threshold.parse() {
+        Ok(value) => value,
+        Err(_) => {
+            return Err(format!("Invalid value of max_threshold {}", max_threshold));
+         }
+    };
+    Ok((enable_fib, max_threshold))
+}
