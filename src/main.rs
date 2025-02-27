c0e4baf202cb84d36890a2d2aae8016c0ce7514a
Benie Joy Possi <155323859+benie-joy-possi@users.noreply.github.com>
2025-02-27 16:04:19 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
     println!("Hello, World!");
-    let enable_fib = env::args().nth(1).unwrap_or_else(|| "false".to_string());
+    let enable_fib = env::args().nth(1).unwrap_or_else(|| "true".to_string());
 
     let max_threshold = env::args().nth(2).and_then(|s| s.parse::<u32>().ok()).unwrap_or(1000);
 
