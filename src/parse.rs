558f076d836c4cf43215fdf9e5d9f8af045c23ad
benie-joy-possi <beniejoypossi@gmail.com>
2025-03-01 17:49:39 +0100

    modified main.rs and parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,7 @@
 use std::{env, format};
 
 pub fn parse() -> Result<(bool, u128), String> {
-    let enable_fib = env::var("enable_fib").unwrap_or_else(|_| "false".to_string());
+    let enable_fib = env::var("enable_fib").unwrap_or_else(|_| "true".to_string());
 
     let max_threshold = env::var("max_threshold").unwrap_or_else(|_| "1000".to_string());
 
